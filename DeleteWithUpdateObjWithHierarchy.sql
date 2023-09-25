7f1a15b9d2902f91124a98e2291ca5f3d605d0ff
Vitālijs Gagunovs <vitalij.gagunov@gmail.com>
2023-09-25 14:03:46 +0300

    Delete With Update

---
+++ DeleteWithUpdateObjWithHierarchy.sql
@@ -0,0 +1,91 @@
+declare @deleteFromTable varchar(max) = 'table_name'
+declare @condition varchar(max) = 'column_id = @id'
+declare @tbl int
+select @tbl = object_id from sys.tables where name = @deleteFromTable
+
+;with x_foreign_keys(base_tbl_id, base_tbl, base_column_id, base_column, dependant_tbl_id, dependant_tbl, dependant_column_id, dependant_column, dependant_nullable)
+as
+(
+	select 
+		referenced_object_id,
+		convert(varchar(max),child_table.name),
+		child_column.column_id,
+		convert(varchar(max),child_column.name),
+		parent_table.object_id,
+		convert(varchar(max),parent_table.name),
+		parent_column.column_id,
+		convert(varchar(max),parent_column.name),
+		parent_column.is_nullable
+	from sys.foreign_key_columns fkc
+	join sys.tables child_table on child_table.object_id = fkc.referenced_object_id
+	join sys.columns parent_column on parent_column.column_id = parent_column_id and parent_column.object_id = fkc.parent_object_id 
+	join sys.columns child_column  on child_column.column_id = referenced_column_id and child_column.object_id = fkc.referenced_object_id
+	join sys.tables parent_table on parent_table.object_id = fkc.parent_object_id
+)
+,x_foreign_joins_delete(base_tbl, dependant_tbl, join_script)
+as
+(
+	select 
+		base_tbl,
+		dependant_tbl,
+		' ###.' + base_column + '= $$$.' + dependant_column 
+	from x_foreign_keys x
+	where dependant_nullable = 0
+)
+,x_foreign_joins_update(base_tbl, dependant_tbl, dependant_column, join_script)
+as
+(
+	select 
+		base_tbl,
+		dependant_tbl,
+		dependant_column,
+		' ###.' + base_column + '= $$$.' + dependant_column 
+	from x_foreign_keys x
+	where dependant_nullable = 1
+)
+,delete_order(rn_parent, rn_txt, tbl, script, script_tail, tbl_path)
+as
+(
+	select 
+		convert(varchar(max),''), 
+		convert(varchar(max),'t1'), 
+		@deleteFromTable,
+		convert(varchar(max),'delete ' + @deleteFromTable + ' where ' + @condition),
+		convert(varchar(max),' where ' + @condition),
+		cast('|' as varchar(max)) + @deleteFromTable + '|'
+	union all
+	select 
+		rn_txt,
+		rn_txt + '_' + convert(varchar(max),row_number() over(order by dependant_tbl)),
+		dependant_tbl,
+		convert(varchar(max),'delete ' + rn_txt + '_' + convert(varchar(max),row_number() over(order by dependant_tbl)) 
+			+ ' from ' + dependant_tbl + ' as ['+rn_txt + '_' + convert(varchar(max),row_number() over(order by dependant_tbl))+'] join ' 
+			+ base_tbl + ' as ['+rn_txt+'] ON ' 
+			+ REPLACE(REPLACE(join_script,'###',rn_txt),'$$$',rn_txt + '_' + convert(varchar(max),row_number() over(order by dependant_tbl))) + ' ' 
+			+ script_tail),
+		convert(varchar(max),' join ' + base_tbl + ' as ['+rn_txt+'] ON ' 
+		+ REPLACE(REPLACE(join_script,'###',rn_txt),'$$$',rn_txt + '_' + convert(varchar(max),row_number() over(order by dependant_tbl))) + ' ' 
+		+ script_tail),
+		tbl_path + dependant_tbl + '|'
+	from delete_order do
+	join x_foreign_joins_delete on tbl= base_tbl
+	where dependant_tbl <> base_tbl
+		and tbl_path not like ('%|' + base_tbl + '|' + dependant_tbl + '|%')
+		and rn_txt not like '%U%'
+	union all
+	select
+		rn_txt, 
+		rn_txt + '_U',
+		dependant_tbl,
+		'update ' + rn_txt + '_U' + ' set ' + dependant_column + ' = NULL FROM ' + dependant_tbl + ' as [' + rn_txt + '_U] join ' + base_tbl + ' as ['+rn_txt+'] ON ' 
+			+ REPLACE(REPLACE(join_script,'###',rn_txt),'$$$',rn_txt + '_' + convert(varchar(max),row_number() over(order by dependant_tbl))) + ' ' 
+			+ script_tail,
+		null,
+		tbl_path + dependant_tbl + '|'
+	from delete_order do
+		join x_foreign_joins_update on tbl = base_tbl
+	where rn_txt not like '%U%'
+)
+select * 
+from delete_order
+order by rn_txt desc
